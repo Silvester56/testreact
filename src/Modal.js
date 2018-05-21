bc3f6af91faef94dc5a142c6f9c09fac1dd0b18e
FF <francois.faucherie@hubbiel.com>
2018-05-21 21:03:54 +0200

    t

---
+++ src/Modal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Modal extends React.Component {
+
+	render() {
+		if (this.props.profil.id) {
+			return (
+				<div>
+				<span onClick={() => this.props.callback()}>Fermer [X]</span><br/>
+				Nom: {this.props.profil.lastname}<br/>
+				Prenom: {this.props.profil.firstname}<br/>
+				Balance	{this.props.profil.balance}<br/>
+				Age: {this.props.profil.age}<br/>
+				Email: {this.props.profil.email}<br/>
+				Adresse: {this.props.profil.address}<br/>
+				Societé: {this.props.profil.company}
+				</div>
+				);
+		} else {
+			return (false);
+		}
+	}
+}
+
+ export default Modal;
